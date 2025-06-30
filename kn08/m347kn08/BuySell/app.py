746a4b8b596cdca349c04a27b21ead1edcd6c2bd
Marco Matteo <marco.matteo@edu.tbz.ch>
2025-06-30 14:57:55 +0200

    fix buysell microservice

--- kn08/m347kn08/BuySell/app.py
+++ kn08/m347kn08/BuySell/app.py
@@ -1,15 +1,17 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
-account_url = "localhost:8080/Account"
+account_url = "http://localhost:8080/Account"
 
-def add_crypto(user_id: int, amount: int):
+def add_crypto(user_id: str, amount: str):
     requests.post(f"{account_url}/AddCrypto?userId={user_id}&amount={amount}")
 
 
-def remove_crypto(user_id: int, amount: int):
+def remove_crypto(user_id: str, amount: str):
     requests.post(f"{account_url}/RemoveCrypto?userId={user_id}&amount={amount}")
 
 
@@ -19,7 +21,7 @@ def buy():
     user_id = data.get('id')
     amount = data.get('amount')
 
-    if user_id is None or amount is None or amount < 0:
+    if user_id is None or amount is None:
         return jsonify(False)
 
 
@@ -32,11 +34,11 @@ def sell():
     user_id = data.get('id')
     amount = data.get('amount')
 
-    if user_id is None or amount is None or amount < 0:
+    if user_id is None or amount is None:
         return jsonify(False)
 
     remove_crypto(user_id, amount)
     return jsonify(True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8002)
